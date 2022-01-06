159e2ce5d1a704fa9924d60d83b85a9ce0bff7b4
Simon Kadisch <simon.kadisch@k13-engineering.at>
2022-01-06 08:48:55 +0100

    WIP

--- websocket-signaling.js
+++ websocket-signaling.js
@@ -21,16 +21,40 @@ const create = ({ logger, url }) => {
     lastEmittedOpen = open;
   };
 
-  const rws = new ReconnectingWebSocket(
-    url,
-    [],
-    {
-      WebSocket,
-      maxReconnectionDelay: 60000,
-    }
-  );
+  const rws = new ReconnectingWebSocket(url, [], {
+    WebSocket,
+    maxReconnectionDelay: 60000,
+  });
 
-  rws.addEventListener("open", () => {
+  rws.addEventListener("ping", () => {
+    logger.log("got ping!");
+  });
+
+  const expectedPingInterval = 10000;
+  const pingTimeout = 3 * expectedPingInterval;
+
+  let pingTimeoutHandle = undefined;
+  const refreshPingTimeout = () => {
+    if (pingTimeoutHandle) {
+      clearTimeout(pingTimeoutHandle);
+    }
+
+    pingTimeoutHandle = setTimeout(() => {
+      pingTimeoutHandle = undefined;
+
+      logger.log("ping timeout, reconnecting...");
+      rws.refresh();
+    }, pingTimeout);
+  };
+
+  rws.addEventListener("open", (event) => {
+    const ws = event.target;
+    ws.on("ping", () => {
+      logger.log("received ping");
+      refreshPingTimeout();
+    });
+    refreshPingTimeout();
+
     logger.log("WebSocket connected");
 
     open = true;
@@ -46,6 +70,9 @@ const create = ({ logger, url }) => {
 
   rws.addEventListener("close", () => {
     logger.warn("WebSocket closed");
+
+    clearTimeout(pingTimeoutHandle);
+    pingTimeoutHandle = undefined;
 
     open = false;
     maybeInformAboutConnection();
